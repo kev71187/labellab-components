5cc7f36d2a846d2b513824e8af0d48ef03efc3f3
kevin woods <kevin@thebitstillery.com>
2019-03-23 11:48:41 -0400

    1.1.2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // const Image = require("./image")
 import preview from "./components/elements/Preview"
 import labeler from "./components/elements/Labeler"
-const version = "1.1.1"
+const version = "1.1.2"
 export const Preview = preview
 export const Labeler = labeler
 export const Version = version
